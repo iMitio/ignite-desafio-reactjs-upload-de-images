4469ed8829194458f0893b1bada63e41f4add857
Mitio <noguchimitio@gmail.com>
2022-07-26 14:44:45 -0300

    Finalizado!

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,46 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      isCentered size="4xl"
+    >
+      <ModalOverlay/>
+      <ModalContent
+        marginX="auto"
+        width="auto"
+        height="auto"
+        maxWidth={['300px', '500px', '900px']}
+        maxHeight={['350px', '450px', '600px']}
+      >
+        <ModalBody
+          padding="0"
+        >
+          <Image
+            src={imgUrl}
+            maxWidth={['300px', '500px', '900px']}
+            maxHeight={['350px', '450px', '600px']}
+          />
+
+        </ModalBody>
+        <ModalFooter
+          bg="pGray.800"
+          height="2rem"
+          paddingY="20px"
+          borderBottomEndRadius="5px"
+        >
+          <Link
+            href={imgUrl}
+            isExternal
+            fontSize="1rem"
+            marginRight="auto"
+          >
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+    );
 }
